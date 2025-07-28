afedbddf2890359723281db1b994ff9f65203c17
AlexPraxedes12 <grigoratech@gmail.com>
2025-07-28 04:16:18 -0600

    No found

--- frontend/learnsynth/lib/screens/audio_recorder_screen.dart
+++ frontend/learnsynth/lib/screens/audio_recorder_screen.dart
@@ -4,6 +4,8 @@ import 'package:record/record.dart';
 import '../widgets/primary_button.dart';
 import '../constants.dart';
 import '../content_provider.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 /// Simple audio recorder using the `record` package.
 class AudioRecorderScreen extends StatefulWidget {
@@ -14,19 +16,41 @@ class AudioRecorderScreen extends StatefulWidget {
 }
 
 class _AudioRecorderScreenState extends State<AudioRecorderScreen> {
-  final Record _record = Record();
+  final _record = AudioRecorder();
   bool _isRecording = false;
+  Future<void> requestMicPermission() async {
+    final status = await Permission.microphone.request();
+    if (!status.isGranted) {
+      // Mostrar alerta o mensaje de error
+      throw Exception("Microphone permission not granted");
+    }
+  }
 
   Future<void> _toggleRecording() async {
     if (_isRecording) {
       final path = await _record.stop();
+
       if (path != null && context.mounted) {
         Provider.of<ContentProvider>(context, listen: false).setAudioPath(path);
         Navigator.pushNamed(context, Routes.loading);
       }
     } else {
-      await _record.start();
+      await requestMicPermission();
+
+      final dir = await getApplicationDocumentsDirectory();
+      final filePath =
+          '${dir.path}/recording_${DateTime.now().millisecondsSinceEpoch}.m4a';
+
+      await _record.start(
+        const RecordConfig(
+          encoder: AudioEncoder.aacLc,
+          bitRate: 128000,
+          sampleRate: 44100,
+        ),
+        path: filePath,
+      );
     }
+
     setState(() => _isRecording = !_isRecording);
   }
 
