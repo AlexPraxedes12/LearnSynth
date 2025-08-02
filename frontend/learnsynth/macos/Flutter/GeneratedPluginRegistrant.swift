bd8e8cd6d2dbdfe0feb810eb8d12d830b83df41f
AlexPraxedes12 <grigoratech@gmail.com>
2025-08-02 00:47:09 -0600

    exit

--- frontend/learnsynth/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/learnsynth/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import ffmpeg_kit_flutter_full_gpl
 import file_picker
 import path_provider_foundation
 import video_compress
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   VideoCompressPlugin.register(with: registry.registrar(forPlugin: "VideoCompressPlugin"))
