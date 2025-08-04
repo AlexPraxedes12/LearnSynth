e3d3a168f508d3dd2d3751ce872ee0eb06e824b1
AlexPraxedes12 <grigoratech@gmail.com>
2025-08-04 03:48:59 -0600

    recibido

--- frontend/learnsynth/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/learnsynth/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import ffmpeg_kit_flutter_min_gpl
 import file_picker
+import speech_to_text_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
 }
