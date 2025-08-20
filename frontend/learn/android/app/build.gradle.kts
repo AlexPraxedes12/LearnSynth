72c87e3af7937b30468d663d49b11eba2db54c17
AlexPraxedes12 <grigoratech@gmail.com>
2025-08-20 17:29:47 -0600

    Refactor transcription pipeline with new ffmpeg and backend

--- frontend/learn/android/app/build.gradle.kts
+++ frontend/learn/android/app/build.gradle.kts
@@ -1,42 +1,25 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "com.example.learn"
     compileSdk = 35
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.learn"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
     }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+    kotlinOptions { jvmTarget = "17" }
 }
 
 flutter {
--- frontend/learn/linux/flutter/generated_plugin_registrant.cc
+++ frontend/learn/linux/flutter/generated_plugin_registrant.cc
@@ -7,13 +7,13 @@
 #include "generated_plugin_registrant.h"
 
 #include <file_selector_linux/file_selector_plugin.h>
-#include <vosk_flutter/vosk_flutter_plugin.h>
+#include <path_provider_linux/path_provider_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) file_selector_linux_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "FileSelectorPlugin");
   file_selector_plugin_register_with_registrar(file_selector_linux_registrar);
-  g_autoptr(FlPluginRegistrar) vosk_flutter_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "VoskFlutterPlugin");
-  vosk_flutter_plugin_register_with_registrar(vosk_flutter_registrar);
+  g_autoptr(FlPluginRegistrar) path_provider_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "PathProviderPlugin");
+  path_provider_plugin_register_with_registrar(path_provider_linux_registrar);
 }
