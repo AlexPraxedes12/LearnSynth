0fc7cff7070ce00a64771d251be3b9554f875ef7
AlexPraxedes12 <grigoratech@gmail.com>
2025-08-19 23:36:47 -0600

    c

--- frontend/learn/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/learn/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import audio_session
-import ffmpeg_kit_flutter
 import file_selector_macos
 import just_audio
 import path_provider_foundation
@@ -14,7 +13,6 @@ import speech_to_text_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
-  FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
